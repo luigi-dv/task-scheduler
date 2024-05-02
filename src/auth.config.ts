c832e2f5aff9a814993f7714cb01e7873b673bbe
Luigi Davila <luigelo@ldvloper.com>
2024-05-02 05:09:26 +0200

    Magic Link Added

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,13 +3,24 @@ import Google from "@auth/core/providers/google";
 import Resend from "@auth/core/providers/resend";
 import { Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import { sendCustomVerificationRequest } from "@/lib/providers/EmailProvider/sendCustomVerificationRequest";
 
 export default {
   providers: [
     GitHub,
     Google,
     Resend({
+      server: process.env.AUTH_RESEND_SERVER,
       from: "no-reply@ldvloper.com",
+      sendVerificationRequest({ identifier, url, provider, theme }) {
+        // Custom sendVerificationRequest function
+        sendCustomVerificationRequest({
+          identifier,
+          url,
+          provider,
+          theme,
+        });
+      },
     }),
   ],
   callbacks: {
---
+++ src/emails/MagicLinkEmail/index.ts
@@ -0,0 +1,1 @@
+export { MagicLinkEmail } from "./MagicLinkEmail";
